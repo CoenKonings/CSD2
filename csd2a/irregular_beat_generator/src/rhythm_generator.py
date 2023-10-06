6406c53f62df904af90c7ca272e6231b25774077
Coen Konings <coen.konings@hotmail.com>
2023-10-06 18:11:38 +0200

    copy bugfix from event based seq

--- csd2a/irregular_beat_generator/src/rhythm_generator.py
+++ csd2a/irregular_beat_generator/src/rhythm_generator.py
@@ -16,26 +16,34 @@ import simpleaudio as sa
 from random import random
 
 
-def generate_hat_pattern(sequencer):
+class RhythmGenerator:
+    """
+    A class to be used to generate rhythms.
+    TODO implement.
+    """
+    pass
+
+
+def generate_high_pattern(sequencer, length):
     """
     Generate a hihat pattern. The hihat pattern is mostly random.
     """
     audio_file = sa.WaveObject.from_wave_file("../assets/hat.wav")
-    track = sequencer.add_track(16, audio_file, "kick")
+    track = sequencer.add_track(length, audio_file, "kick")
 
     for i in range(16):
         if random() < 0.75:
             track.add_note(i, 1, 100)
 
 
-def generate_snare_pattern(sequencer):
+def generate_mid_pattern(sequencer, length):
     """
     Generate a snare pattern. Each measure contains at least one snare that is
     played on the 2nd or 4th beat, and has a 50% chance to containe one snare
     that is played a sixteenth note early.
     """
     audio_file = sa.WaveObject.from_wave_file("../assets/snare.wav")
-    track = sequencer.add_track(16, audio_file, "snare")
+    track = sequencer.add_track(length, audio_file, "snare")
     offset_snare_present = False
 
     for i in range(16):
@@ -44,13 +52,13 @@ def generate_snare_pattern(sequencer):
             track.add_note(j, 1, 100)
 
 
-def generate_kick_pattern(sequencer):
+def generate_low_pattern(sequencer, length):
     """
     Generate a kick pattern. A kick is always played on the first sixteenth of
     a measure.
     """
     audio_file = sa.WaveObject.from_wave_file("../assets/kick.wav")
-    track = sequencer.add_track(16, audio_file, "kick")
+    track = sequencer.add_track(length, audio_file, "kick")
 
     for i in range(16):
         if i == 0 or random() < 0.2:
@@ -63,9 +71,9 @@ def main():
     """
     sequencer = Sequencer()
     sequencer.set_bpm(random() * 60 + 70)
-    generate_kick_pattern(sequencer)
-    generate_snare_pattern(sequencer)
-    generate_hat_pattern(sequencer)
+    generate_low_pattern(sequencer, 16)
+    generate_mid_pattern(sequencer, 16)
+    generate_high_pattern(sequencer, 13)
     sequencer.start()
 
 
