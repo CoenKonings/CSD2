fed7085b3f2f3b90a79b4ad7d6b80d1344aa3038
Coen Konings <coen.konings@hotmail.com>
2023-10-09 14:59:24 +0200

    markov from rhythm af (oeps vergeten tussendoor te committen)

--- csd2a/irregular_beat_generator/src/markov.py
+++ csd2a/irregular_beat_generator/src/markov.py
@@ -7,16 +7,17 @@ Contains the necessary classes to generate a polyphonic rhythm using a Markov
 chain.
 """
 from random import random
+import simpleaudio as sa
+import time
 
 
 class Node:
-    def __init__(self, name, duration):
+    def __init__(self, name):
         self.name = name
-        self.duration = duration
         self.edges = []
 
     def __str__(self):
-        return "Node {}, duration {}.".format(self.name, self.duration)
+        return "Node {}.".format(self.name)
 
     def add_edge(self, node, value):
         """
@@ -58,15 +59,16 @@ class MarkovChain:
     """
     def __init__(self):
         self.nodes = []
+        self.state = None
 
-    def add_node(self, name, duration):
+    def add_node(self, name):
         """
         Add a new node to the Markov chain.
         """
-        if any(node.name == name for node in self.nodes):
+        if self.node_exists(name):
             raise Exception("A node with name {} already exists.".format(name))
 
-        self.nodes.append(Node(name, duration))
+        self.nodes.append(Node(name))
 
     def add_edge_by_node_index(self, node_1, node_2, value):
         """
@@ -90,3 +92,108 @@ class MarkovChain:
             raise Exception("Node with name {} does not exist.".format(node_2_name))
 
         node_1.add_edge(node_2, value)
+
+    def node_exists(self, name):
+        return any(node.name == name for node in self.nodes)
+
+    def step(self):
+        """
+        Change the chain's state.
+        """
+
+        if not self.state:
+            self.state = self.nodes[0]
+        else:
+            self.state = self.state.follow_random_edge()
+
+        sounds = self.state.name.split("&")
+
+        for sound in sounds:
+            if sound == "low":
+                sa.WaveObject.from_wave_file("../assets/kick.wav").play()
+            elif sound == "mid":
+                sa.WaveObject.from_wave_file("../assets/snare.wav").play()
+            elif sound == "high":
+                sa.WaveObject.from_wave_file("../assets/hat.wav").play()
+
+
+def markov_chain_from_rhythm_file():
+    """
+    Read a rhythm from a file and generate a markov chain.
+    TODO: clean up this mess
+    """
+    markov_chain = MarkovChain()
+
+    with open("markov_input.txt") as input_file:
+        lines = [line for line in input_file]
+
+    rhythm = {}
+    total_length = 0
+
+    for line in lines:
+        name, part = line.split()
+        rhythm[name] = [*part]
+
+        if len(rhythm[name]) > total_length:
+            total_length = len(rhythm[name])
+
+    onsets = []
+
+    for i in range(total_length):
+        node_name = ""
+
+        for key in rhythm.keys():
+            if rhythm[key][i] == "x":
+                node_name = key if node_name == "" else node_name + "&" + key
+
+        onsets.append(node_name)
+
+    edges = {}
+    onset = onsets.pop(0)
+
+    while len(onsets) != 0:
+        if onset in edges.keys():
+            edges[onset].append(onsets[0])
+        else:
+            edges[onset] = [onsets[0]]
+
+        onset = onsets.pop(0)
+
+    for node_name in edges.keys():
+        markov_chain.add_node(node_name)
+
+    for from_node_name in edges.keys():
+        for to_node_name in edges.keys():
+            percent = edges[from_node_name].count(to_node_name) / len(edges[from_node_name])
+            markov_chain.add_edge_by_node_name(from_node_name, to_node_name, percent)
+
+    return markov_chain
+
+
+def main():
+    mode = input("Select the mode. 1 for markov chain from rhythm, 2 for markov chain from file.\n>")
+    markov_chain = None
+
+    if mode == "1":
+        markov_chain = markov_chain_from_rhythm_file()
+    elif mode == "2":
+        pass
+    else:
+        print("Invalid mode.")
+        exit()
+
+    start_time = time.time()
+    bpm = 120
+    sixteenth_duration = 15 / bpm
+    i = 0
+
+    while True:
+        if time.time() - start_time >= i * sixteenth_duration:
+            markov_chain.step()
+            i += 1
+
+        time.sleep(0.001)
+
+
+if __name__ == "__main__":
+    main()
